2c534cf11092ea1a647b659a1e7fdfe80781df6a
Nguyen Tuan Anh <seabird86@gmail.com>
2024-12-05 21:27:03 +1000

    footer

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 ng build --base-href=/myProfile/
+rm -rf /Users/kris/Projects/myProfile/docs/*
 cp -R /Users/kris/Projects/profile/dist/profile/browser/* /Users/kris/Projects/myProfile/docs
 cp /Users/kris/Projects/myProfile/docs/index.html /Users/kris/Projects/myProfile/docs/404.html

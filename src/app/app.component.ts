ec8e073d1f8389c308bcb2327fed20638f44c38e
Nguyen Tuan Anh <seabird86@gmail.com>
2024-12-06 23:04:16 +1000

    updage 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatMenuModule, MatIconModule, RouterLink],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    imports: [RouterOutlet, MatButtonModule, MatMenuModule, MatIconModule, RouterLink],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'profile';
--- src/app/public/home/home.component.ts
+++ src/app/public/home/home.component.ts
@@ -2,11 +2,10 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [MatCardModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+    selector: 'app-home',
+    imports: [MatCardModule],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 

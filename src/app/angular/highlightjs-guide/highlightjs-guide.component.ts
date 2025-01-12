ca5eb102d4b908718776dcdd9cb7b417d358511b
Nguyen Tuan Anh <seabird86@gmail.com>
2025-01-12 10:44:04 +1000

    font , line-heigh

--- src/app/angular/highlightjs-guide/highlightjs-guide.component.ts
+++ src/app/angular/highlightjs-guide/highlightjs-guide.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HighlightDirective } from '@app/lib/directive/highlight.directive';
+import { BlogComponent } from "../../lib/component/blog/blog.component";
 
 @Component({
   selector: 'app-highlightjs-guide',
-  imports: [HighlightDirective],
+  imports: [HighlightDirective, BlogComponent],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './highlightjs-guide.component.html'
 })
 export class HighlightjsGuideComponent {

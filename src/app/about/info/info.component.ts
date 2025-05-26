b2ba5ddc44ebb583ebbc0c1b26d9bf20d0e3f838
Nguyen Tuan Anh <seabird86@gmail.com>
2025-05-26 22:53:47 +1000

    info page

--- src/app/about/info/info.component.ts
+++ src/app/about/info/info.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-info',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })

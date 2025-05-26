dd8b0f3fa17fb38bcd482f2995a2bb24965b528c
Nguyen Tuan Anh <seabird86@gmail.com>
2025-05-26 22:50:58 +1000

    Info page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { CasourelBlogComponent } from '@app/blog/angular/casourel/casourel-blog.
 import { HighlightjsBlogComponent } from '@app/blog/angular/highlightjs/highlightjs-blog.component';
 import { ContentTableBlogComponent } from '@app/blog/angular/content-table/content-table-blog.component';
 import { FireflyBlogComponent } from '@app/blog/angular/firefly/firefly-blog.component';
+import { InfoComponent } from './about/info/info.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'blog/angular/casourel', component: CasourelBlogComponent },
     { path: 'blog/angular/highlightjs', component: HighlightjsBlogComponent },
     { path: 'blog/angular/content-table', component: ContentTableBlogComponent },
-    { path: 'blog/angular/firefly', component: FireflyBlogComponent }
+    { path: 'blog/angular/firefly', component: FireflyBlogComponent },
+    { path: 'about/info', component: InfoComponent },
 ];
